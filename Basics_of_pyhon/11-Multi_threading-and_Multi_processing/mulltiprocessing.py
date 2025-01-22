8966ba57735e47140e5c525bf590ed73713a0388
Mahadev1729 <devcampus17@gmail.com>
2025-01-22 23:35:05 +0530

    multiprocessing

--- Basics_of_pyhon/11-Multi_threading-and_Multi_processing/mulltiprocessing.py
+++ Basics_of_pyhon/11-Multi_threading-and_Multi_processing/mulltiprocessing.py
@@ -5,3 +5,31 @@
 import mulltiprocessing
 
 import time
+
+def square_number():
+    for i in range(5):
+        time.sleep(1)
+        print(f"{i} Square:{i*i}")
+
+def cube_number():
+    for i in range(5):
+        time.sleep(1.5)
+        print(f"{i} cube:{i*i*i}")
+        
+if __name__=="__main__":# entry point
+# square_number()
+# cube_number()
+## create two process
+    pl=mulltiprocessing.Process(target=square_number)
+    p2=mulltiprocessing.Process(target=cube_number)
+
+    t1=time.time()
+
+    pl.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
+
+    finished_time=time.time()-target
+    print(finished_time)
