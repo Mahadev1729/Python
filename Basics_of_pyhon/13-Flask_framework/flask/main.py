5dc1352982525ed59c37218f48352584d7fdbb15
Mahadev1729 <devcampus17@gmail.com>
2025-01-26 14:42:36 +0530

    add some more pages

--- Basics_of_pyhon/13-Flask_framework/flask/main.py
+++ Basics_of_pyhon/13-Flask_framework/flask/main.py
@@ -7,13 +7,21 @@ which will be yout WSGI
 '''
 app=Flask(__name__) 
 
-@app.route('/') ## home page route
+@app.route("/start")
+def welcome_start():
+    return render_template("start.html")
+
+@app.route('/home') ## home page route
 def welcome():
     return render_template("home.html")
 
 @app.route("/index")
 def welcome_index():
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/login')
 def welocome_login():
