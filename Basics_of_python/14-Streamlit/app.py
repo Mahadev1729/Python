82c0897f5c903ee6ad7d586a34de44ddffa5f064
Mahadev1729 <devcampus17@gmail.com>
2025-02-23 00:45:33 +0530

    start with streamlit

--- Basics_of_python/14-Streamlit/app.py
+++ Basics_of_python/14-Streamlit/app.py
@@ -4,3 +4,19 @@ import numpy as np
 
 ## Title of the application
 st.title("hello streamlit")
+
+## display simple text
+
+st.write("This is a simple")
+
+## for dataframe
+
+df=pd.DataFrame({
+    'first col':[1,2,3,4],
+    'second col':[10,20,30,40]
+})
+
+## display the dataframe
+
+st.write("Here is dataframe")
+st.write(df)
