d4988f02d4e8f4c3003b765a5cb2bafb45bd801b
Mahadev1729 <devcampus17@gmail.com>
2025-03-02 00:50:30 +0530

    selecting process

--- Basics_of_python/14-Streamlit/widgets.py
+++ Basics_of_python/14-Streamlit/widgets.py
@@ -6,9 +6,20 @@ name=st.text_input("Enter your name")
 
 age=st.slider("Select your age:",0,100,25)
 
+options= [
+    "Select below one","Python", "JavaScript", "Java", "C", "C++", "C#", "Swift", "Go", "Rust", 
+    "Kotlin", "TypeScript", "Ruby", "PHP", "Perl", "R", "Dart", "Scala", 
+    "Haskell", "Lua", "Elixir", "Clojure", "F#", "Objective-C", "Visual Basic",
+    "MATLAB", "Julia", "Shell", "Bash", "PowerShell", "Groovy", "Erlang",
+    "Lisp", "Scheme", "Prolog", "COBOL", "Fortran", "Ada", "Pascal", "ABAP",
+    "RPG", "ActionScript", "ML", "OCaml", "Tcl", "Smalltalk", "Crystal",
+    "Forth", "PL/SQL", "FoxPro", "Modula-2", "Delphi", "Hack"
+]
+
+choice=st.selectbox("Choose your favorite language:",options)
+st.write(f"You selected {choice}")
 
 st.write(f"your age is {age}")
-if name:
-    st.write(f"hello,{name}")
-else:
-    st.write(f"Hello,unknown")    
+
+
+ 
