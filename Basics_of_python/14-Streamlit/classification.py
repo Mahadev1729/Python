3985fe917c38d03a48107a8327f25b34ba0be8a5
Mahadev1729 <devcampus17@gmail.com>
2025-03-10 23:08:29 +0530

    complete with streamlit

--- Basics_of_python/14-Streamlit/classification.py
+++ Basics_of_python/14-Streamlit/classification.py
@@ -1,3 +1,19 @@
 import streamlit as slt
+import pandas as pd 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris  
 
-slt.write("I am obed")
+@slt.cache
+def load_data():
+    iris=load_iris()
+    df=pd.DataFrame(iris,data,columns=iris.feature_names)
+    df['species']=iris.target
+    return df,iris.target_names
+
+df,target_name=load_data()
+
+model=RandomForestClassifier()
+model.fit(df.iloc[:,:-1],df['species'])
+
+slt.sidebar.title("Input Features")
+
